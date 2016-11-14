72ae86727f38c26fe883ff7b13498d5a1d5600af
pyk <pyksoft@gmail.com>
2016-11-14 01:11:37 +0100

    adds basic UI

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+  public void LoadLevel(string levelName) {
+    SceneManager.LoadScene (levelName);
+  }
+
+  public void QuitRequest() {
+
+    // Application.Quit(); is an anti-pattern on mobile
+    // the application state should be managed by the OS and the User
+    Application.Quit();
+  }
+
+}
